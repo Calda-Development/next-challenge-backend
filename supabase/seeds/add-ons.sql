1b6398fa71d93bdb42b5a24dbf4563b0e2de2a00
tomaz-calda <tomaz@thecalda.com>
2025-10-20 11:41:50 +0200

    Add seeds

---
+++ supabase/seeds/add-ons.sql
@@ -0,0 +1,13 @@
+insert into
+  public.add_ons (name, price)
+values
+  ('Ketchup', 100),
+  ('Mozzarella Fior di Latte', 150),
+  ('Gorgonzola', 150),
+  ('Mushrooms', 150),
+  ('Pesto', 200),
+  ('Salami', 200),
+  ('Pineapple', 200),
+  ('Smoked Chicken', 300),
+  ('Ricotta', 150),
+  ('Ham', 200);
---
+++ supabase/seeds/pizzas.sql
@@ -0,0 +1,87 @@
+insert into
+  public.pizzas (name, price, image_url, description, type)
+values
+  (
+    'PIZZA MARGARITA',
+    1199,
+    'https://i.imgur.com/07N5MMP.png',
+    'San Marzano tomato sauce, fresh mozzarella, basil leaves, extra virgin olive oil, sea salt.',
+    'vegetarian'
+  ),
+  (
+    'Pepperoni Inferno',
+    1299,
+    'https://i.imgur.com/4V4gnfu.png',
+    'Spicy tomato sauce, mozzarella, pepperoni, red chili flakes, roasted red peppers, oregano.',
+    'spicy'
+  ),
+  (
+    'GARDEN GODDESS',
+    1099,
+    'https://i.imgur.com/aaaaa.png',
+    'Tomato sauce, vegan mozzarella, cherry tomatoes, bell peppers, olives, zucchini, red onion, fresh basil.',
+    'vegan'
+  ),
+  (
+    'TRUFFLE TEMPTATION',
+    1550,
+    'https://i.imgur.com/07N5MMP.png',
+    'White truffle cream, mozzarella, mushrooms, caramelized onions, parmesan, arugula drizzle.',
+    'meat'
+  ),
+  (
+    'BBQ BLAZE',
+    1450,
+    'https://i.imgur.com/hybkv6T.png',
+    'BBQ sauce base, smoked chicken, red onions, mozzarella, cheddar, cilantro, and a drizzle of BBQ glaze.',
+    'meat'
+  ),
+  (
+    'MEDITERRANEAN DREAM',
+    1199,
+    null,
+    'Tomato sauce, mozzarella, feta, sun-dried tomatoes, kalamata olives, spinach, oregano, olive oil drizzle.',
+    'vegan'
+  ),
+  (
+    'The meat maestro',
+    1399,
+    'https://i.imgur.com/tBkbQsA.png',
+    'Tomato sauce, mozzarella, pepperoni, sausage, ham, bacon, ground beef, garlic, herbs.',
+    'meat'
+  ),
+  (
+    'Four Cheese Symphony',
+    1199,
+    'https://i.imgur.com/YHjLA5B.png',
+    'Mozzarella, gorgonzola, parmesan, ricotta, garlic butter base, and a sprinkle of black pepper.',
+    'vegetarian'
+  ),
+  (
+    'Hawaiian Heatwave',
+    1350,
+    'https://i.imgur.com/tmZnUQL.png',
+    'Tomato sauce, mozzarella, ham, pineapple, jalapeños, and sweet chili glaze.',
+    'meat'
+  ),
+  (
+    'Pesto Paradise',
+    1399,
+    'https://i.imgur.com/tBkbQsA.png',
+    null,
+    'vegetarian'
+  ),
+  (
+    'Spicy Diablo',
+    1499,
+    'https://i.imgur.com/tmZnUQL.png',
+    'Fiery tomato sauce, mozzarella, spicy salami, hot peppers, onions, and chili oil drizzle.',
+    'spicy'
+  ),
+  (
+    'White Wonderland',
+    999,
+    'https://i.imgur.com/YHjLA5B.png',
+    'Garlic cream sauce, mozzarella, ricotta, spinach, mushrooms, and a dusting of parmesan.',
+    'vegan'
+  );
